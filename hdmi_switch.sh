b19f04c5c7c207732da12bf1dfe3b5aec4d27369
Simbd <10208905+simbd@users.noreply.github.com>
2020-04-08 11:44:43 +0200

    Update hdmi_switch.sh

--- hdmi_switch.sh
+++ hdmi_switch.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
-# Script crée par Simbd
+## cf tuto pour le son => https://lehollandaisvolant.net/?d=2017/06/10/23/37/39-linux-changer-de-sortie-audio-en-un-clic
 
-#######################################
-IN="DVI-I-1"       # écran interne (pc)
-EXT="HDMI-1"       # écran externe (sortie hdmi)
-#######################################
+### pacmd list | grep "active profile"
+#active profile: <output:hdmi-stereo>
 
-# prérequis : avoir la présence du répértoire/fichier suivant au départ : ~/.script/hdmi_off 
+### pacmd list | grep "active port" | grep "output"
+#active port: <hdmi-output-0>
 
-if [ -f ~/.script/hdmi_off ]
-then
-  #Activation HDMI
-  xrandr --output $IN --off --output $EXT --auto
-  pacmd set-card-profile 0 output:hdmi-stereo #audio hdmi activé
-  pacmd set-card-profile 1 off   #audio pc désactivé
-  mv ~/.script/hdmi_off ~/.script/hdmi_on
-else
-  #Désactivation HDMI
-  xrandr --output $EXT --off --output $IN --auto
-  pacmd set-card-profile 0 off
-  pacmd set-card-profile 1 output:analog-stereo #audio pc activé (audio hdmi forcément désactivé)
-  mv ~/.script/hdmi_on ~/.script/hdmi_off
-fi
+### pacmd list-sinks | grep "name:"
+#name: <alsa_output.pci-0000_00_1f.3.hdmi-stereo>
+
+#############
+IN="eDP1"  # écran pc portable
+EXT="DP1" # écran externe relié en HDMI
+#############
+
+# Switch affichage vers écran externe
+xrandr --output $IN --off --output $EXT --auto
+
+# Switch carte son vers écran externe
+pacmd set-card-profile 0 off
+pacmd set-card-profile 0 output:hdmi-stereo
+pacmd set-sink-port alsa_output.pci-0000_00_1f.3.hdmi-stereo
+
+
+## Pour rebasculer HDMI sur Off, il n'y a que l'affichage à faire (le son bascule auto)
+# xrandr --output $EXT --off --output $IN --auto
+
+
+exit
